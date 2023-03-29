f83e47c9a8d4641603cbb4672ba26a5691f6819f
naeemtalukder <naeemtalukder169135@gmail.com>
2023-03-30 00:29:45 +0600

    SignUp create user

--- src/Pages/Login/SignUp.js
+++ src/Pages/Login/SignUp.js
@@ -1,6 +1,7 @@
-import React from "react";
+import React, { useContext } from "react";
 import { useForm } from "react-hook-form";
 import { Link } from "react-router-dom";
+import { AuthContext } from "../../contexts/AuthProvider";
 
 const SignUp = () => {
   const {
@@ -8,8 +9,15 @@ const SignUp = () => {
     formState: { errors },
     handleSubmit,
   } = useForm();
+  const {createUser} = useContext(AuthContext);
   const handleSignUp = (data) => {
     console.log(data);
+    createUser(data.email, data.password)
+    .then(result => {
+      const user = result.user;
+      console.log(user);
+    })
+    .catch(error => console.log(error));
   };
   return (
     <div className="h-[650px] flex justify-center items-center">
@@ -22,8 +30,7 @@ const SignUp = () => {
             </label>
             <input
               type="text"
-              {...register("name", { required: "Type your Name",
-              pattern: /^[A-Za-z]+$/i })}
+              {...register("name", { required: "Type your Name"})}
               aria-invalid={errors.firstName ? "true" : "false"}
               className="input input-bordered w-full max-w-xs"
             />
@@ -68,12 +75,10 @@ const SignUp = () => {
                 {errors.password?.message}
               </p>
             )}{" "}
-            <label className="label mb-1">
-              <Link className="label-text-alt">Forget Password?</Link>
-            </label>
+
           </div>
           <input
-            className="btn btn-accent w-full mb-1"
+            className="btn btn-accent w-full mb-1 mt-4"
             value="sign up"
             type="submit"
           />
